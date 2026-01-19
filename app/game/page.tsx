863e71756e3ed37c2ab5c974acdfc7dca8b5dfad
TripleSmeven <74826504+TripleSmeven@users.noreply.github.com>
2026-01-19 00:50:07 -0500

    initial home page

---
+++ app/game/page.tsx
@@ -0,0 +1,10 @@
+import styles from './game.module.css'
+
+
+export default function Game() {
+  return (
+    <div className={styles.gameParent}>
+
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,43 @@
-import Image from "next/image";
+import styles from './home.module.css';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className={styles.homeParent}>
+      <h1>Murder At The Occult Club</h1>
+      <div>A detective puzzle game by TripleSmeven</div>
+
+      <h2>About this game</h2>
+      <div>
+        In this puzzle game, you&apos;ll have to solve the mystery of who murdered Zach Cunningham. Read through the evidence carefully and leave no stone unturned!
+      </div>
+      <br />
+      <div>
+        This game was inspired by murder mystery detective board games like the Hunt a Killer series or Unsolved Case Files. I also took many ideas from puzzle games like The Roottrees Are Dead and Case of the Golden Idol. I hope you enjoy the game!
+      </div>
+      <br />
+      <div>
+        This game is completely free, with no ads, signups, or payment required. I made it purely to express my creativity and for the enjoyment of designing my own game.
+      </div>
+
+      <h2>How to play</h2>
+      <div>
+        The goal of the game is to find out who killed Zach Cunningham, how they did it, and why. First, read the opening letter and the police report to familiarize yourself with the situation.
+      </div>
+      <br />
+      <div>
+        Then, start reading over the evidence. Use the tabs on the left side and the arrows to scroll through different types of evidence.
+      </div>
+      <br />
+      <div>
+        Use your deduction skills to name the owner of each piece of evidence. You can also use the notepad on the right hand side to jot down notes. The notepad is saved between sessions (just don&apos;t clear your localstorage).
+      </div>
+      <br />
+      <div>
+        Finally, you must name the killer as well as their motivation and method. You win if you deduce correctly. Good luck!
+      </div>
+
+      <h2>Feedback</h2>
+      <div>If you have any feedback, please message me on discord or reddit @triplesmeven</div>
     </div>
   );
 }
